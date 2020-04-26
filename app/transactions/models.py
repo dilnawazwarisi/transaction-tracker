9846988f28c2b005bcc826ae51636baf3f0e94b2
Dilnawaz Warisi <dilnawazwarisi@91springboard.com>
2020-04-27 03:50:01 +0530

    Added transaction model and updated requirements

--- app/transactions/models.py
+++ app/transactions/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
 
-# Create your models here.
+
+class Transaction(MPTTModel):
+    transaction_id = models.BigIntegerField(unique=True,null=False)
+    amount = models.DecimalField(decimal_places=2, max_digits=11)
+    type = models.CharField(max_length=500,null=False)
+    parent = models.ForeignKey('self',on_delete=models.CASCADE,blank=True,null=True,related_name='children')
+
+    class Meta:
+        db_table = "transaction"
+
+
+
